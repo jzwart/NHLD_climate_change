aef58f8be90da1291048f62f2f8e22eead62669f
jzwart <jzwart@usgs.gov>
2018-05-23 16:03:50 -0500

    doc analysis and qa changes

--- 1_data/src/qa_scenarios.R
+++ 1_data/src/qa_scenarios.R
@@ -1,48 +1,48 @@
-# checks for all lakes in each scenario 
+# checks for all lakes in each scenario
 
 scenarios_qa <- function(ind_file, scenarios_ind_file, retro_ind_file, remake_file, gd_config){
   # check to see if all lakes are in each scenario
-  out = list()
-  
+  out = data_frame()
+
   scenarios_agg <- readRDS(sc_retrieve(scenarios_ind_file, remake_file = remake_file))
-  
+
   retro_agg <- readRDS(sc_retrieve(retro_ind_file, remake_file = remake_file))
-  
+
   scenarios_agg[[25]] <- retro_agg[[1]]
   scenarios_agg[[26]] <- retro_agg[[2]]
   names(scenarios_agg)[c(25:26)] <- c('Retro_all', 'Retro_sum')
-  
+
   dir <- 'D:/MyPapers/NHLD Climate Change/Results/C_model_output/Condor_Results/'
   dir_lookup <- 'D:/MyPapers/NHLD Climate Change/Results/C_model_output/'
-  
+
   scenarios <- list.files(dir)
   scenario_lookup <- read.csv(file.path(dir_lookup,'scenarios.csv'),
-                              stringsAsFactors = F) # 
-  
+                              stringsAsFactors = F) #
+
   lakes <- c()
   for(i in 1:length(scenario_lookup$scenario)){
     cur <- scenarios_agg[[i]]
     lakes <- c(lakes, cur$Permanent_)
   }
-  lakes <- lakes[!duplicated(lakes)] # all lakes among sceario runs 
-  
-  for(i in 1:length(scenario_lookup$scenario)){ # lakes common to every scenario run 
+  lakes <- lakes[!duplicated(lakes)] # all lakes among sceario runs
+
+  for(i in 1:length(scenario_lookup$scenario)){ # lakes common to every scenario run
     cur <- scenarios_agg[[i]]
     lakes <- lakes[lakes%in%cur$Permanent_]
   }
-  
+
   scenario_lookup<-rbind(scenario_lookup,c(12,'Retro'))
-  for(i in 1:length(scenario_lookup$scenario)){ # keeping only lakes common to every scenario run 
+  for(i in 1:length(scenario_lookup$scenario)){ # keeping only lakes common to every scenario run
     q = i + (i-1)
-    
+
     cur <- scenarios_agg[[q]]
     curSum <- scenarios_agg[[q+1]]
-    
+
     cur <- cur[cur$Permanent_%in%lakes,]
     curSum <- curSum[curSum$Permanent_%in%lakes,]
     cur <- cur[sort.list(cur$Permanent_),]
     curSum  <- curSum[sort.list(curSum$Permanent_),]
-    
+
     cur$alk <- ifelse(cur$GWin+cur$Baseflow>0,10^(3.2815*(1-exp(-1.2765*log10(cur$GWin+cur$Baseflow+cur$SWin)/3.2815))),0)
     curSum$alk <- cur$alk
     cur$HRT <- cur$Vol/(cur$GWin + cur$SWin + cur$DirectP + cur$Baseflow + cur$IceMelt)
@@ -51,14 +51,14 @@ scenarios_qa <- function(ind_file, scenarios_ind_file, retro_ind_file, remake_fi
     curSum$HRT_woEvap <- curSum$Vol / (curSum$GWout + curSum$SWout)
     cur$FracRet <- 1 - (cur$DOC_export / cur$DOC_Load)
     curSum$FracRet <- 1 - (curSum$DOC_export / curSum$DOC_Load)
-    cur$DIC_load <- cur$GWin * 0.7025 + cur$SWin * 0.9041667 + cur$Baseflow * 0.9041667 + cur$DirectP * 0.0833333 + cur$IceMelt * 0.01360082 # mol C day-1 
-    curSum$DIC_load <- curSum$GWin * 0.7025 + curSum$SWin * 0.9041667 + curSum$Baseflow * 0.9041667 + curSum$DirectP * 0.0833333 + curSum$IceMelt * 0.01360082 # mol C day-1 
-    cur$sed_resp <- cur$Sed_phyto * 0.75 + cur$Sed_tPOC * 0.1 # 75% of phyto C and 10% of tpoc C was converted to co2 
-    curSum$sed_resp <- curSum$Sed_phyto * 0.75 + curSum$Sed_tPOC * 0.1 # 75% of phyto C and 10% of tpoc C was converted to co2 
+    cur$DIC_load <- cur$GWin * 0.7025 + cur$SWin * 0.9041667 + cur$Baseflow * 0.9041667 + cur$DirectP * 0.0833333 + cur$IceMelt * 0.01360082 # mol C day-1
+    curSum$DIC_load <- curSum$GWin * 0.7025 + curSum$SWin * 0.9041667 + curSum$Baseflow * 0.9041667 + curSum$DirectP * 0.0833333 + curSum$IceMelt * 0.01360082 # mol C day-1
+    cur$sed_resp <- cur$Sed_phyto * 0.75 + cur$Sed_tPOC * 0.1 # 75% of phyto C and 10% of tpoc C was converted to co2
+    curSum$sed_resp <- curSum$Sed_phyto * 0.75 + curSum$Sed_tPOC * 0.1 # 75% of phyto C and 10% of tpoc C was converted to co2
     cur$waterIn <- cur$GWin + cur$SWin + cur$DirectP + cur$Baseflow + cur$IceMelt
     curSum$waterIn <- curSum$GWin + curSum$SWin + curSum$DirectP + curSum$Baseflow + curSum$IceMelt
     cur$fluvialOut <- cur$GWout + cur$SWout # m3 day-1
-    curSum$fluvialOut <- curSum$GWout + curSum$SWout 
+    curSum$fluvialOut <- curSum$GWout + curSum$SWout
     cur$dicLoadvResp <- cur$DIC_load / (cur$DOC_Respired + cur$sed_resp)
     curSum$dicLoadvResp <- curSum$DIC_load / (curSum$DOC_Respired + curSum$sed_resp)
     cur$percentEvap <- cur$LakeE / (cur$LakeE + cur$GWout + cur$SWout)
@@ -69,15 +69,15 @@ scenarios_qa <- function(ind_file, scenarios_ind_file, retro_ind_file, remake_fi
     curSum$pco2 <- 0.952 * curSum$fracCO2 * curSum$DIC_epi / curSum$Vepi * 1000 * 29.41
     cur$doc_conc <- (cur$DOCr_epi+cur$DOCl_epi+cur$DOCr_hypo+cur$DOCl_hypo)/cur$Vol*12
     curSum$doc_conc <- (curSum$DOCr_epi+curSum$DOCl_epi+curSum$DOCr_hypo+curSum$DOCl_hypo)/curSum$Vol*12
-    
+
     lakeSizeBins <- c(0,0.01,.1,1,10,100)*1e6 # breaks for max cutoff of lake size from Downing et al. 2006
     cur$lakeSizeBins <- cut(cur$Area, breaks = lakeSizeBins)
     curSum$lakeSizeBins <- cut(curSum$Area, breaks = lakeSizeBins)
-    cur$period <- ifelse(scenario_lookup$scenario[i] == 'Retro', 
-                         'Retro', 
+    cur$period <- ifelse(scenario_lookup$scenario[i] == 'Retro',
+                         'Retro',
                          ifelse(length(grep('2080s', scenario_lookup$scenario[i]))>0, '2080s', '2050s'))
-    curSum$period <- ifelse(scenario_lookup$scenario[i] == 'Retro', 
-                         'Retro', 
+    curSum$period <- ifelse(scenario_lookup$scenario[i] == 'Retro',
+                         'Retro',
                          ifelse(length(grep('2080s', scenario_lookup$scenario[i]))>0, '2080s', '2050s'))
     cur$season <- 'all'
     curSum$season <- 'open_water'
@@ -89,15 +89,17 @@ scenarios_qa <- function(ind_file, scenarios_ind_file, retro_ind_file, remake_fi
       curSum$gcm <- 'Retro'
     }
 
-    out[[q]] <- cur
-    out[[q+1]] <- curSum
-    
-    out_name_all = paste(scenario_lookup$scenario[i], 'all', sep = '_')
-    out_name_sum = paste(scenario_lookup$scenario[i], 'sum', sep = '_') 
-    
-    names(out)[q:(q+1)] <- c(out_name_all, out_name_sum)
+    # out[[q]] <- cur
+    # out[[q+1]] <- curSum
+    #
+    # out_name_all = paste(scenario_lookup$scenario[i], 'all', sep = '_')
+    # out_name_sum = paste(scenario_lookup$scenario[i], 'sum', sep = '_')
+    #
+    # names(out)[q:(q+1)] <- c(out_name_all, out_name_sum)
+
+    out <- bind_rows(out, cur, curSum)
   }
-  
+
   data_file = as_data_file(ind_file)
   saveRDS(out, data_file)
   gd_put(remote_ind = ind_file, local_source = data_file, config_file = gd_config)
--- 2_analysis/src/change_in_var.R
+++ 2_analysis/src/change_in_var.R
@@ -1,16 +1,48 @@
-# general function for pulling out variables of interest and seeing change in response to scenarios 
+# general function for pulling out variables of interest and seeing change in response to scenarios
 
 change_in_var <- function(ind_file, raw_ind_file, remake_file, var_cfg_file, gd_config){
-  
+
   all_results <- readRDS(sc_retrieve(raw_ind_file, remake_file = remake_file))
-  
-  var_cfg_file <- yaml::yaml.load_file(var_cfg_file)
-  
-  results <- all_results[grep(period,names(all_results))] %>% 
-    bind_rows()
-  
-  retro <- all_results[grep('Retro', names(all_results))]
-  
-  for
-  
+
+  var_cfg <- yaml::yaml.load_file(var_cfg_file) # indicates which periods, seasons, and variable we want returned
+
+  if(var_cfg$variable == 'doc'){
+    out <- all_results %>%
+
+      select('Permanent_', 'period', 'season', 'gcm',
+             grep(var_cfg$variable, tolower(colnames(.))),
+             -grep('respired', tolower(colnames(.))),
+             -grep('export', tolower(colnames(.))),
+             -grep('load', tolower(colnames(.))),
+             'Vepi', 'Vhypo', 'Vol') %>%
+      mutate(doc_epi = (DOCr_epi + DOCl_epi) / Vepi * 12,
+              doc_hypo = (DOCr_hypo + DOCl_hypo) / Vepi * 12,
+              doc_all = (DOCr_epi + DOCl_epi + DOCr_hypo + DOCl_hypo) / Vol *12) %>%
+      group_by(Permanent_, period, season) %>%
+      summarise(mean_doc_epi = mean(doc_epi),
+                mean_doc_hypo = mean(doc_hypo),
+                mean_doc_all = mean(doc_all)) %>%
+      ungroup()
+
+    retro <- out %>%
+      dplyr::filter(period == 'Retro') %>%
+      select(-period) %>%
+      rename(retro_doc_epi = mean_doc_epi,
+             retro_doc_hypo = mean_doc_hypo,
+             retro_doc_all = mean_doc_all)
+
+    scenarios <- out %>%
+      dplyr::filter(period != 'Retro') %>%
+      left_join(y = retro, by = c('Permanent_', 'season')) %>%
+      mutate(delta_doc_epi = mean_doc_epi - retro_doc_epi,
+             delta_doc_hypo = mean_doc_hypo - retro_doc_hypo,
+             delta_doc_all = mean_doc_all - retro_doc_all,
+             ratio_doc_epi = mean_doc_epi / retro_doc_epi,
+             ratio_doc_hypo = mean_doc_hypo / retro_doc_hypo,
+             ratio_doc_all = mean_doc_all / retro_doc_all)
+  }
+
+  data_file = as_data_file(ind_file)
+  saveRDS(scenarios, data_file)
+  gd_put(remote_ind = ind_file, local_source = data_file, config_file = gd_config)
 }

1a23b6244d9003381ae2c854b02d3a07cf493e77
jzwart <jzwart@usgs.gov>
2019-06-11 15:52:11 -0400

    changing file names

--- 8_publish_data/src/collate_scenario.R
+++ 8_publish_data/src/collate_scenario.R
@@ -61,81 +61,62 @@ collate_scenario <- function(data_file,
   }) %>% bind_rows()
 
   saveRDS(out, data_file)
+
 }
 
 # collating the retro model runs
 
-collate_retro <- function(ind_file, gd_config){
-  # for present day results
+collate_retro <- function(data_file,
+                          forcing_file,
+                          results_dir,
+                          gd_config){
 
-  list_out <- list()
-
-  dir <- 'D:/MyPapers/NHLD Climate Change/Results/C_model_output/Present/20170819/'
+  # for retrospecitive model runs
 
   skip=6*365 # days to skip; first 6 years (spin up)
 
-  watersheds<-read.table('1_data/in/NHLDsheds_20170323.txt',
-                         stringsAsFactors = F,
-                         header=T,
-                         sep = '\t')
-
   threshold <- 0.05 # if volume of lake is below X% of original volume, don't include this in analyses because DOC / kD / etc.. were blowing up at low volumes
 
-  cur_scenario <- 'Present'
+  forcing_lookup = read.csv(forcing_file, stringsAsFactors = F)
 
-  files <- list.files(dir)
+  cur_scenario <- 'retro'
+
+  files <- list.files(results_dir)
   if(length(files[grep('adj',files)])>0){
     files <- files[-grep('adj',files)]
   }
 
-  sum <- data.frame() # open ice only
-  all <- data.frame()
-  for(j in 1:length(files)){ # length(files
-    print(c(i,j))
+  out = lapply(files, function(cur_file){
+    print(cur_file)
 
-    cur <- readRDS(file.path(dir,files[j]))
-    lake<-strsplit(tolower(files[j]),'_c_model.rds',fixed = T)[[1]]
+    cur <- readRDS(file.path(results_dir, cur_file))
+    lake <- strsplit(tolower(cur_file),'_c_model.rds',fixed = T)[[1]]
 
-    cur<-na.omit(cur)
+    cur <- na.omit(cur)
     if(length(cur$time)<2){
       next
     }
     lake <- toupper(lake) # making all uppercase; important for JAZ and ZJH lakes
 
-    cur<-cur[skip:(nrow(cur)-0),3:ncol(cur)] # skipping first X number of days
-    cur<-cur[cur$Vol>0,] # only keeping days when there's actually water
-    curSum=cur[cur$LakeE>0,] # only open ice periods
-    ndays_open_ice <- nrow(curSum)
-    ndays_ice <- nrow(cur[cur$LakeE==0,])
+    cur <- cur[skip:(nrow(cur)-0), ] # skipping first X number of days
 
     # removing outliers based on % of original volume
-    vol_frac <- cur$Vol/cur$Vol[1]
-    cur <- cur[vol_frac>threshold,]
+    vol_frac <- cur$Vol / cur$Vol[1]
+    cur <- cur[vol_frac > threshold, ]
 
-    cur<-data.frame(t(apply(cur,MARGIN = 2,FUN = mean)))
-    curSum<-data.frame(t(apply(curSum,MARGIN = 2,FUN = mean)))
-    cur$Permanent_<-lake
-    curSum$Permanent_<-lake
-    curSum$ndays_open_ice <- ndays_open_ice
-    curSum$ndays_ice <- ndays_ice
-    cur$ndays_open_ice <- ndays_open_ice
-    cur$ndays_ice <- ndays_ice
+    cur_force = forcing_lookup$force[forcing_lookup$currID == lake]
 
-    sum<-rbind(sum,curSum)
-    all<-rbind(all,cur)
-  }
-  sum <- merge(sum, watersheds, by='Permanent_')
-  all <- merge(all, watersheds, by='Permanent_')
+    cols_to_get_rid_of = c('IceSnow', 'LandMelt', 'SWoutMinusLandMelt', 'LandMeltEst', 'Radius')
+    cols_not_to_round = c('time', 'datetime', 'Permanent_', 'forcing_file', 'emergent_d_epi', 'emergent_d_hypo', 'GPP')
 
-  out_name_all = paste(cur_scenario, 'all', sep = '_')
-  out_name_sum = paste(cur_scenario, 'sum', sep = '_')
+    cur <- cur %>%
+      mutate(Permanent_ = lake,
+             forcing_file = cur_force) %>%
+      select(-cols_to_get_rid_of) %>%
+      mutate_at(colnames(.)[!colnames(.) %in% cols_not_to_round], round, 3)
 
-  list_out[[1]] <- all
-  list_out[[2]] <- sum
+  }) %>% bind_rows()
 
-  names(list_out)[1:2] <- c(out_name_all, out_name_sum)
+  saveRDS(out, data_file)
 
-  data_file = as_data_file(ind_file)
-  saveRDS(list_out, data_file)
-  gd_put(remote_ind = ind_file, local_source = data_file, config_file = gd_config)
 }
